4d78c4ee663db837df11b2b892832d5700bafa0e
devyash_tanwar <tanwardevyash@gmail.com>
2024-06-30 19:15:36 +0530

    exporting skeletons for the application

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,17 @@
-export { Header } from "./header";
+import UpcomingEvents from './home/upcoming-events';
+import DealsChart from './home/deals-chart';
+import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+import AccordionHeaderSkeleton from './skeleton/accordion-header';
+import KanbanColumnSkeleton from './skeleton/kanban';
+import LatestActivitiesSkeleton from './skeleton/latest-activities';
+import ProjectCardSkeleton from './skeleton/project-card';
+
+export {
+    UpcomingEvents,
+    DealsChart,
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    LatestActivitiesSkeleton,
+    ProjectCardSkeleton,
+};
